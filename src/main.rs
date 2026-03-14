cbcbe842f2a85df68bf416ea00f49ec03cd48fe6
Ben Karl <bkarl@tonkintaylor.co.nz>
2026-03-15 09:23:55 +1300

    FIrst dev iteration of get_num_rows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let csv_path = Path::new("data.csv");
+    let num_rows = get_num_rows(&csv_path).unwrap();
+    println!("Number of rows: {}", num_rows);
+}
+
+fn get_num_rows(csv_path: &Path) -> Result<i32, csv::Error> {
+    // Create a CSV reader from the file path. A Reader needs to be mutable, because
+    // reading from it changes its internal state.
+    let mut reader = csv::Reader::from_path(csv_path)?;
+    Ok(reader.records().count() as i32)
 }
