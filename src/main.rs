1305cd3c6852996f0dca49a69c83a99c714e93ef
Ben Karl <bkarl@tonkintaylor.co.nz>
2026-03-15 10:04:35 +1300

    Write get_num_cols

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use std::path::Path;
 
 fn main() {
-    let csv_path = Path::new("data.csv");
+    let csv_path = Path::new("test_data/sample.csv");
     let num_rows = get_num_rows(&csv_path).unwrap();
+    let num_cols = get_num_cols(&csv_path).unwrap();
     println!("Number of rows: {}", num_rows);
+    println!("Number of columns: {}", num_cols);
 }
 
 fn get_num_rows(csv_path: &Path) -> Result<i32, csv::Error> {
@@ -11,6 +13,12 @@ fn get_num_rows(csv_path: &Path) -> Result<i32, csv::Error> {
     // reading from it changes its internal state.
     let mut reader = csv::Reader::from_path(csv_path)?;
     Ok(reader.records().count() as i32)
+}
+fn get_num_cols(csv_path: &Path) -> Result<i32, csv::Error> {
+    let mut reader = csv::Reader::from_path(csv_path)?;
+    // .headers() returns a reference to a StringRecord of the headers. A StringRecord
+    // is basically a vector if strings representing a row of the CSV.
+    Ok(reader.headers()?.len() as i32)
 }
 
 #[cfg(test)]
@@ -32,6 +40,22 @@ mod tests {
         fn file_not_found() {
             let path = Path::new("test_data/nonexistent.csv");
             let result = get_num_rows(path);
+            assert!(result.is_err()); // should return an error, not panic
+        }
+    }
+    mod test_get_num_cols {
+        use super::*;
+
+        #[test]
+        fn happy_path() {
+            let path = Path::new("test_data/sample.csv");
+            let result = get_num_cols(path).unwrap();
+            assert_eq!(result, 3); // 3 columns: name, age, city
+        }
+        #[test]
+        fn file_not_found() {
+            let path = Path::new("test_data/nonexistent.csv");
+            let result = get_num_cols(path);
             assert!(result.is_err()); // should return an error, not panic
         }
     }
